d383491479dbf7281c4fdc35570e1e69c70ebafa
Knomo Seikei <knomolestes@gmail.com>
2015-11-09 10:08:46 -0500

    "<>/" sign missings

--- challenge239_easy_a-game-of-threes/gameofthrees.cpp
+++ challenge239_easy_a-game-of-threes/gameofthrees.cpp
@@ -1,26 +1,25 @@
-#include iostream
+#include <iostream>
 
 using namespace std;
 
 int main ()
 {
     int n, mod;
-    cin  n;
     n = 31337357;
-    cout  n   ;
+    cout << n << " ";
     while (n != 1){
         mod = n % 3;
         if (mod & 1){
-            cout  -1 ;
+            cout << "-1" ;
             --n;
         } else if (mod == 2) {
-            cout  1;
+            cout << "1";
             ++n;
         } else {
-            cout  0;
+            cout << "0";
         }
-        n = 3;
-        cout  endl  n   ;
+        n /= 3;
+        cout << endl << n << " ";
     }
     return 0;
 }
